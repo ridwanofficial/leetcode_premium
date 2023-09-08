f5acff28d81f8ae28cc9d4d67d62d651f000ef88
Ridwan <ridwan123.official@gmail.com>
2023-09-09 00:47:36 +0600

    added

---
+++ 418-Sentence Screen Fitting.js
@@ -0,0 +1,19 @@
+/*
+
+rows = 2, cols = 8, sentence = ["hello", "world"]
+
+no word split
+order remains 
+two words need a space 
+
+len > rows  
+
+*********
+********* 
+
+current cols/word > 1
+    yes => add word one by one with space untill whole word fit into row 
+    No  => go to next row then do the same thing 
+
+
+*/
