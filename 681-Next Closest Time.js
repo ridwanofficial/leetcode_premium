d07c6ddcf93a70ff7f108ca10b6e5f2aab30a21c
Ridwan <ridwan123.official@gmail.com>
2023-09-09 00:25:34 +0600

    added

---
+++ 681-Next Closest Time.js
@@ -0,0 +1,21 @@
+/*
+
+|00-23|:|00-59|
+
+18:59
+
+
+1. sort available digits  [1,3,4,9]
+then start from last digit 
+
+function find next bigger(available digits,digitCount){
+   19:34 =>  1 => binary search found => 9 
+   19:39 =>  2 digit => 3 => 4 | recursive lowest possible of other number all 1
+   18:59 =>  3 digit => 8=> 9 | 19:11
+   23:59 =>  4 digit => all lowest
+
+}
+
+max 4 digit, range 1-4
+
+*/
