1158a153fb9d30dc7380dcd175aee0a19a88b78b
Ridwan <ridwan123.official@gmail.com>
2023-09-15 14:19:25 +0600

    ADDED

---
+++ 294-Flip Game II.js
@@ -0,0 +1,26 @@
+/*
+++++  consecutive 3 or 4 will guarantee a win
+
++++++ loss 
+
+6 reduce to 4 but 2/2 
+6 reduce to 3
+
+is about all about 2/2/2
+
+**_**_***
+
+
+two ways
+delete left and right only 3 or 4
+*_*
+greater then 4 only one delete possible 
+or not 
+
+for each dp there are [n/2] ceil possibilities 
+_**
+
+
+LAST OF ALL IT IS BRUTEFORCE JUST SIMULATION !!!!
+
+*/
